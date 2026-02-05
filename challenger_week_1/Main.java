96789c3837e3e7df1b8ca17e466977c250dabc47
abinercosta-cl <abinerclcosta@gmail.com>
2026-02-05 17:03:09 -0300

    Setup inicial: Ambiente Java

--- challenger_week_1/Main.java
+++ challenger_week_1/Main.java
@@ -1,5 +1,17 @@
+/* Na aula de Hoje iniciaremos com um dos 5 pilares da Programação Tipos de Dados ou "Data Type" */
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Word");
+        int age = 25;
+        double salario = 1500.50;
+        boolean isEstudante = true;
+        char genero = 'M';
+        String nome = "Abiner";
+
+        System.out.println(age);
+        System.out.println(salario);
+        System.out.println(isEstudante);
+        System.out.println(genero);
+        System.out.println(nome);
     }
 }
---
+++ challenger_week_1/readme.md
@@ -0,0 +1,1 @@
+# Variaveis e Tipos de Dados em Java
