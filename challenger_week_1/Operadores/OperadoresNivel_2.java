c6e545e506cc7887894b436e009256cb5de62382
abinercosta-cl <abinerclcosta@gmail.com>
2026-02-13 18:22:14 -0300

    Adcionado exercicio 2 de operadores

---
+++ challenger_week_1/Operadores/OperadoresNivel_2.java
@@ -0,0 +1,11 @@
+public class OperadoresNivel_2 {
+    public static void main(String[] arg) {
+        int vidas = 3;
+
+        vidas--;
+        vidas--;
+        vidas++;
+
+        System.out.println(vidas);
+    }
+}
