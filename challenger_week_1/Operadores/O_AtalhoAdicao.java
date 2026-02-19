a7aa77ca4ed42800b879a029bbfb7a0fd1899641
abinercosta-cl <abinerclcosta@gmail.com>
2026-02-19 13:18:58 -0300

    Entendendo o atalho incremento ++

---
+++ challenger_week_1/Operadores/O_AtalhoAdicao.java
@@ -0,0 +1,11 @@
+public class O_AtalhoAdicao {
+    public static void main(String[] args) {
+        int vidas = 3;
+
+        vidas--;// Perdeu uma vida
+        vidas--;// Perdeu mais uma
+        vidas++;// Ganhou mais uma
+
+        System.out.println(vidas);
+    }
+}
